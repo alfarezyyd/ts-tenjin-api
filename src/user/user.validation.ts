f014518d8b2983a09807f38825d8fee69ee28f8a
alfarezyyd <adityaalfarezyd@gmail.com>
2024-03-30 19:08:59 +0700

    refactor: using function for convert

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,19 +1,17 @@
 import { z, ZodType } from 'zod';
 import { GenderEnum } from './enum/gender.enum';
+import { ConvertHelper } from '../helper/convert.helper';
 
 export class UserValidation {
   static readonly CREATE: ZodType = z.object({
     name: z.string().min(1).max(255),
     gender: z.string().transform((arg, ctx) => {
-      if (!Object.values(GenderEnum).includes(arg.toUpperCase())) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: 'User gender not valid',
-        });
-        return z.NEVER;
-      } else {
-        return GenderEnum[arg];
-      }
+      return ConvertHelper.convertStringIntoEnum(
+        arg,
+        ctx,
+        'Product condition not valid',
+        GenderEnum,
+      );
     }),
     email: z.string().min(1).max(255),
     telephone: z.string().min(1).max(13),
