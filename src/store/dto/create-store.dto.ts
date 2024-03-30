8d7835e6ff57c3feb81d6062dc3078b33a623c7e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-03-30 17:43:25 +0700

    feat: create-store DTO

---
+++ src/store/dto/create-store.dto.ts
@@ -0,0 +1,10 @@
+export class CreateStoreDto {
+  name: string;
+  domain: string;
+  slogan: string;
+  locationName: string;
+  city: string;
+  zipCode: string;
+  detail: string;
+  description: string;
+}
