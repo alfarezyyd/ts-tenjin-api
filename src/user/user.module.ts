cf278c4d5f31a1643b72d6d8c2c368fd8e5eb934
alfarezyyd <adityaalfarezyd@gmail.com>
2024-04-12 17:32:01 +0700

    feat: create user to elasticSearch

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { CommonModule } from '../common/common.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,12 +5,14 @@ import ValidationService from '../common/validation.service';
 import { UserValidation } from './user.validation';
 import PrismaService from '../common/prisma.service';
 import { User, userGender } from '@prisma/client';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly validationService: ValidationService,
     private readonly prismaService: PrismaService,
+    private readonly httpService: HttpService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<string> {
@@ -18,18 +20,46 @@ export class UserService {
       UserValidation.CREATE,
       createUserDto,
     );
-    const userPrisma = await this.prismaService.user.create({
-      data: {
-        ...createUserRequest,
-        gender: userGender[createUserDto.gender],
-      },
-    });
+    const userPrisma = await this.prismaService.user
+      .create({
+        data: {
+          ...createUserRequest,
+          gender: userGender[createUserDto.gender],
+        },
+      })
+      .catch((reason) => {
+        throw new HttpException(reason.message, 400);
+      });
 
-    await this.prismaService.cart.create({
-      data: {
-        userId: userPrisma.id,
-      },
-    });
+    await this.prismaService.cart
+      .create({
+        data: {
+          userId: userPrisma.id,
+        },
+      })
+      .catch((reason) => {
+        throw new HttpException(reason.message, 400);
+      });
+
+    await this.httpService
+      .axiosRef({
+        method: 'post',
+        url: `http://localhost:9200/zenith_users/_create/${userPrisma.id}`,
+        headers: {
+          contentType: 'application/json',
+        },
+        data: {
+          name: userPrisma.name,
+          gender: userPrisma.gender,
+          email: userPrisma.email,
+          telephone: userPrisma.telephone,
+          pin: userPrisma.pin,
+          photo_path: userPrisma.photoPath,
+        },
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     return 'Success! new user has been created';
   }
 
