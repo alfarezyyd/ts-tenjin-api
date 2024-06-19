9d38fd7f8bfc55810579eba709450ffdbed81da9
alfarezyyd <adityaalfarezyd@gmail.com>
2024-06-19 15:55:02 +0700

    feat: update schema & delete unnecessary line

--- Database Console.sql
+++ Database Console.sql
@@ -1,12 +1,4 @@
 DROP DATABASE IF EXISTS ts_tenjin_api;
 CREATE DATABASE ts_tenjin_api;
 USE ts_tenjin_api;
-SELECT *
-FROM users;
-SELECT * FROM addresses;
-SELECT * FROM stores;
-SELECT * FROM products;
-SELECT * FROM product_resources;
-SELECT * FROM products_carts;
-SELECT * FROM expedition_provinces;
-SELECT * FROM expedition_cities;
+SHOW TABLES;
