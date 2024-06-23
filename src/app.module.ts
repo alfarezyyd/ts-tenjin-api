9e07fce6668cfbeff9dbaeb5ca53f4c5cd04e0b0
alfarezyyd <adityaalfarezyd@gmail.com>
2024-06-23 08:25:38 +0700

    feat: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { MentorModule } from './mentor/mentor.module';
 import { EducationModule } from './education/education.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     CommonModule,
     UserModule,
     MentorModule,
