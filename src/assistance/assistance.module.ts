18e0a90305bcbfe0ed2a7f3dde61cd04fd739368
alfarezyyd <adityaalfarezyd@gmail.com>
2024-07-25 15:07:06 +0700

    fix: minor bug

--- src/assistance/assistance.module.ts
+++ src/assistance/assistance.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AssistanceService } from './assistance.service';
 import { AssistanceController } from './assistance.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [AssistanceController],
   providers: [AssistanceService],
+  exports: [AssistanceService],
 })
 export class AssistanceModule {}
