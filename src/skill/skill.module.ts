f9d1acb82f6b9d4bc610b5b4d98396901130ce7c
alfarezyyd <adityaalfarezyd@gmail.com>
2024-07-25 15:06:47 +0700

    refactor: optimize import

--- src/skill/skill.module.ts
+++ src/skill/skill.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SkillService } from './skill.service';
 import { SkillController } from './skill.controller';
-import ValidationService from '../common/validation.service';
-import PrismaService from '../common/prisma.service';
 import { CommonModule } from '../common/common.module';
 
 @Module({
