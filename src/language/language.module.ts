25d9583ad3e2fd635f9984280aa8ac1d92622388
alfarezyyd <adityaalfarezyd@gmail.com>
2024-08-17 14:58:51 +0700

    feat: import common module

--- src/language/language.module.ts
+++ src/language/language.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LanguageService } from './language.service';
 import { LanguageController } from './language.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [LanguageController],
   providers: [LanguageService],
 })
