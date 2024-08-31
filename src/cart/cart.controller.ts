6f0ca92e5a38c3891c7211aa049fa9f07017b8d7
alfarezyyd <adityaalfarezyd@gmail.com>
2024-08-31 14:42:04 +0700

    feat: dto

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
 
-@Controller('cart')
+@Controller('carts')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,1 +1,4 @@
-export class CreateCartDto {}
+export class CreateCartDto {
+  assistanceId: number;
+  userId: number;
+}
