327dcbfb15fa06dc3c4520cb114f65c2f19cd04e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-09-01 05:38:32 +0700

    feat: delete unnecessary method

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -37,18 +37,8 @@ export class CartController {
     };
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cartService.findOne(+id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
     return this.cartService.update(+id, updateCartDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cartService.remove(+id);
   }
 }
