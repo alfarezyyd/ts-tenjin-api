130d47a0640635a5787c8f26fc0c536285ce7c8f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-10 00:54:01 +0700

    feat: import jwt module

--- src/mentor/mentor.module.ts
+++ src/mentor/mentor.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MentorService } from './mentor.service';
 import { MentorController } from './mentor.controller';
 import { CommonModule } from '../common/common.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, JwtModule],
   controllers: [MentorController],
   providers: [MentorService],
 })
