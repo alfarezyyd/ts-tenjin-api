015baf75d2a3e6b7515b99ab2bd6a942ecc503df
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-09 22:05:33 +0700

    fix: import module

--- src/withdraw/withdraw.module.ts
+++ src/withdraw/withdraw.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WithdrawService } from './withdraw.service';
 import { WithdrawController } from './withdraw.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [WithdrawController],
   providers: [WithdrawService],
 })
