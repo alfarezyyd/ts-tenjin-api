3cc404226eaa3fed7847d69c2d4e437013faf02d
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-18 13:15:59 +0700

    fix: jwt module missing key

--- src/mentor/mentor.module.ts
+++ src/mentor/mentor.module.ts
@@ -3,9 +3,20 @@ import { MentorService } from './mentor.service';
 import { MentorController } from './mentor.controller';
 import { CommonModule } from '../common/common.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule, JwtModule],
+  imports: [
+    CommonModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1200m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MentorController],
   providers: [MentorService],
 })
