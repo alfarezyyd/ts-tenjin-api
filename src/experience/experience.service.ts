b097e3386e62262e2e931f86f58744c24f47683d
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-24 18:37:50 +0700

    fix: minor bug

--- src/experience/experience.service.ts
+++ src/experience/experience.service.ts
@@ -238,7 +238,7 @@ export class ExperienceService {
       await prismaTransaction.experienceResource.createMany({
         data: allExperienceResourcePayload,
       });
-      if (deletedFilesName !== null) {
+      if (deletedFilesName !== undefined && deletedFilesName?.length > 0) {
         for (const deletedFileName of deletedFilesName) {
           fs.stat(
             `${this.configService.get<string>('MULTER_DEST')}/experience-resources/${this.expressRequest['user']['mentorId']}/${experienceId}/${deletedFileName}`,
