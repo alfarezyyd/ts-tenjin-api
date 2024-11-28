2c5cccccc9a5fe5bcfd913504b36707b52f764cc
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-28 23:29:12 +0700

    feat: mentor module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { CommonModule } from '../common/common.module';
 import { ConfigModule } from '@nestjs/config';
+import { MentorModule } from '../mentor/mentor.module';
 
 @Module({
-  imports: [CommonModule, ConfigModule],
+  imports: [CommonModule, ConfigModule, MentorModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
