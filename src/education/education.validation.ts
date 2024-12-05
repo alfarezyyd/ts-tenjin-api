3b6170f9404fb3a4b0bb243f438c7759d78b7ba5
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-05 11:35:14 +0700

    feat: add validation

--- src/education/education.validation.ts
+++ src/education/education.validation.ts
@@ -1,26 +1,38 @@
 import { z, ZodType } from 'zod';
 
 export default class EducationValidation {
-  static readonly SAVE: ZodType = z.object({
-    name: z.string().max(255).min(5),
-    degree: z.string().max(255).min(3),
-    studyField: z.string().max(255).min(5),
-    startDate: z
-      .string()
-      .refine((val) => !isNaN(Date.parse(val)), {
-        message: 'Invalid Date Format',
-      })
-      .transform((val) => new Date(val)),
-    endDate: z
-      .string()
-      .refine((val) => !isNaN(Date.parse(val)), {
-        message: 'Invalid Date Format',
-      })
-      .transform((val) => new Date(val)),
-    activity: z.string(),
-    society: z.string(),
-    description: z.string().max(255).min(10),
-  });
+  static readonly SAVE: ZodType = z
+    .object({
+      name: z.string().max(255).min(5),
+      degree: z.string().max(255).min(3),
+      studyField: z.string().max(255).min(5),
+      startDate: z
+        .string()
+        .refine((val) => !isNaN(Date.parse(val)), {
+          message: 'Invalid Date Format',
+        })
+        .transform((val) => new Date(val)),
+      endDate: z
+        .string()
+        .refine((val) => !isNaN(Date.parse(val)), {
+          message: 'Invalid Date Format',
+        })
+        .transform((val) => new Date(val)),
+      activity: z.string(),
+      society: z.string(),
+      description: z.string(),
+    })
+    .superRefine((arg, ctx) => {
+      const start = new Date(arg.startDate);
+      const end = new Date(arg.endDate);
+      if (start > end) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['startDate'], // Bisa juga diubah ke "endDate" tergantung preferensi
+          message: 'Start date cannot be after end date',
+        });
+      }
+    });
 
   static readonly UPDATE: ZodType = z.union([
     EducationValidation.SAVE,
